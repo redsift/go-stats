612b730d9b345a0e5711cac72a785cad29d4f59e
csucu <ceysunsucu@gmail.com>
2020-11-09 12:43:09 +0000

    use md5 instead

--- tracing/trace.go
+++ tracing/trace.go
@@ -2,11 +2,9 @@ package tracer
 
 import (
 	"context"
-	"encoding/hex"
+	"crypto/md5"
 	"errors"
 	"fmt"
-
-	"github.com/pierrec/xxHash/xxHash64"
 	"go.opentelemetry.io/otel/api/global"
 	"go.opentelemetry.io/otel/api/trace"
 	"go.opentelemetry.io/otel/exporters/otlp"
@@ -14,6 +12,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/semconv"
+	"io"
 )
 
 func InitTracingProvider(collectorAddress string, serviceName string) (func(), error) {
@@ -66,14 +65,28 @@ func NewRootSpanWithID(ctx context.Context, id string, traceFlags byte) (context
 }
 
 func asTraceID(s string) (trace.ID, error) {
-	hash := xxHash64.Checksum([]byte(s), 0xCAFEBABE)
-	hx := hex.EncodeToString(i64tob(hash))
+	//hash := xxHash64.Checksum([]byte(s), 0xCAFEBABE)
+	//hx := hex.EncodeToString(i64tob(hash))
 
-	var id trace.ID
-	id, err := trace.IDFromHex(hx)
-	if err != nil {
-		return id, err
-	}
+	////fmt.Println("hash: ", len(hash))
+	//fmt.Println("i64tob(hash): ", len(i64tob(hash)))
+	//fmt.Println("i32tob(hash): ", len(i32tob( xxHash32.Checksum([]byte(s), 0xCAFEBABE))))
+	//fmt.Println("hx: ", len(hx))
+	//fmt.Println(hx)
+
+
+	//var id trace.ID
+	//id, err := trace.IDFromHex(hx)
+	//if err != nil {
+	//	return id, err
+	//}
+
+	h := md5.New()
+	_, _ = io.WriteString(h, s)
+	b := h.Sum(nil)
+
+	id := trace.ID{}
+	copy(id[:],b)
 
 	return id, nil
 }
@@ -82,6 +95,14 @@ func i64tob(val uint64) []byte {
 	r := make([]byte, 8)
 	for i := uint64(0); i < 8; i++ {
 		r[i] = byte((val >> (i * 8)) & 0xff)
+	}
+	return r
+}
+
+func i32tob(val uint32) []byte {
+	r := make([]byte, 4)
+	for i := uint32(0); i < 4; i++ {
+		r[i] = byte((val >> (8 * i)) & 0xff)
 	}
 	return r
 }
