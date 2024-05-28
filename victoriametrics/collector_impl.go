df6ec4684c073f777d394bd915b506cdf2479e3c
Julian Picht <julian.picht@redsift.io>
2024-05-28 12:42:24 +0200

    victoriametrics: set up default behavior to fail in a safe manner

--- victoriametrics/collector_impl.go
+++ victoriametrics/collector_impl.go
@@ -2,6 +2,7 @@ package victoriametrics
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"slices"
@@ -10,7 +11,12 @@ import (
 	"golang.org/x/exp/maps"
 )
 
-func (c *Collector) Flush() {
+func (c *Collector) Flush() error {
+	c.flush()
+	return errors.Join(c.Errors()...)
+}
+
+func (c *Collector) flush() {
 	c.swapLock.Lock()
 	defer c.swapLock.Unlock()
 	defer c.buf.Reset()
@@ -27,18 +33,18 @@ func (c *Collector) Flush() {
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.endpoint, c.buf)
 	if err != nil {
-		c.errors = append(c.errors, err)
+		c.err(err)
 		return
 	}
 
 	res, err := c.httpClient.Do(req)
 	if err != nil {
-		c.errors = append(c.errors, err)
+		c.err(err)
 		return
 	}
 
 	if res.StatusCode >= 400 {
-		c.errors = append(c.errors, fmt.Errorf("API returned error status code %d: %s", res.StatusCode, res.Status))
+		c.err(fmt.Errorf("API returned error status code %d: %s", res.StatusCode, res.Status))
 	}
 }
 
@@ -46,7 +52,7 @@ func (c *Collector) render(metrics map[string]*Metric) {
 	for _, metric := range metrics {
 		line, err := metric.MarshalJSON()
 		if err != nil {
-			c.errors = append(c.errors, fmt.Errorf("cannot marshal metric %q: %w", metric.Name, err))
+			c.err(fmt.Errorf("cannot marshal metric %q: %w", metric.Name, err))
 			continue
 		}
 		c.buf.Write(line)
@@ -55,6 +61,10 @@ func (c *Collector) render(metrics map[string]*Metric) {
 }
 
 func (c *Collector) value(stat string, value float64, tags []string) {
+	if c.stopOnErr && c.errc() > 0 {
+		return
+	}
+
 	slices.Sort(tags)
 	key := stat + "|" + strings.Join(tags, "|")
 
@@ -62,9 +72,35 @@ func (c *Collector) value(stat string, value float64, tags []string) {
 	defer c.lock.Unlock()
 	m, ok := c.a[key]
 	if !ok {
+		if c.maxMetrics > 0 && len(c.a) >= c.maxMetrics {
+			c.err(fmt.Errorf("too many metrics, discarding %q (%v)", stat, tags))
+			return
+		}
+
 		m = &Metric{Name: stat}
 		c.a[key] = m
 		m.addLegacyTags(tags)
 	}
+
+	if c.maxPoints > 0 && len(m.Data.Values) >= c.maxPoints {
+		c.err(fmt.Errorf("too many points in %q (%v)", stat, tags))
+		return
+	}
 	m.AddValue(c.clock.Now(), value)
+}
+
+func (c *Collector) err(err error) {
+	if err == nil {
+		return
+	}
+	c.errCond.L.Lock()
+	c.errors = append(c.errors, err)
+	c.errCond.L.Unlock()
+	c.errCond.Broadcast()
+}
+
+func (c *Collector) errc() int {
+	c.errCond.L.Lock()
+	defer c.errCond.L.Unlock()
+	return len(c.errors)
 }
